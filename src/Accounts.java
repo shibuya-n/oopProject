b0478de19e393509ff1bcf550b3eb0e1cc401b30
shibuya-n <a.duong2006@gmail.com>
2022-11-09 21:17:39 -0800

    update

--- src/Accounts.java
+++ src/Accounts.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Accounts {
     public int balance;
     public String accountType;
@@ -7,5 +9,27 @@ public class Accounts {
         this.accountType = accountType;
 
     }
+    public static void menu(){
+        Scanner scannerObject = new Scanner(System.in);
+        System.out.println("[What kind of account would you like to make?] (CHECKING/SAVINGS/EXIT)");
+        String userAnswer = scannerObject.nextLine().trim().toLowerCase();
+
+        oopProject.exit();
+        if (userAnswer.equals("checking")){
+            System.out.println("Checking");
+        }
+
+        else if (userAnswer.equals("savings")){
+            System.out.println("Savings");
+        }
+        else {
+            System.out.print("[ERROR. PLEASE TRY AGAIN]");
+            menu();
+        }
+    }
+    public static void deposit(){
+
+    }
+
 
 }
--- src/oopProject.java
+++ src/oopProject.java
@@ -1,34 +1,65 @@
 import java.util.*;
-
 public class oopProject {
+    public static String userAnswer = " ";
     public static void main(String[] args){
         ask();
     }
 
     public static void ask(){
             System.out.println("[WELCOME TO BaNK]");
-            Scanner scannerObject = new Scanner(System.in);
-            System.out.println("[What kind of account would you like to make?] (CHECKING/SAVINGS/EXIT)");
-            String userAnswer = scannerObject.nextLine().trim().toLowerCase();
+            Scanner menuAsk = new Scanner(System.in);
+            System.out.println("[What would you like to do today?");
+            System.out.println("1. Enter Checking Account");
+            System.out.println("2. Enter Savings Account");
+            System.out.println("3. Make a new account");
+            System.out.println("4. Exit");
+            int menuAnswer = Integer.parseInt(String.valueOf(menuAsk.nextLine()));
 
-            if (userAnswer.equals("checking")){
+            exit();
+            if (menuAnswer == 1) {
                 System.out.println("Checking");
             }
-
-            else if (userAnswer.equals("savings")){
+            else if (menuAnswer == 2){
                 System.out.println("Savings");
             }
-
-            else if (userAnswer.equals("exit")){
-                System.out.println("Thank you for banking with BaNK");
-                System.exit(0);
+            else if (menuAnswer == 3){
+                System.out.println("New Account");
             }
-            else {
-                System.out.print("[ERROR. PLEASE TRY AGAIN]");
+
                 ask();
             }
+
+    public static void accountCreate() {
+        Scanner accountAsk = new Scanner(System.in);
+        System.out.println("[What kind of account would you like to make?] (CHECKING/SAVINGS/EXIT)");
+        userAnswer += accountAsk.nextLine().trim().toLowerCase();
+
+        if (userAnswer.equals("checking")) {
+            userAnswer = " ";
+            System.out.println("Checking");
         }
 
+        else if (userAnswer.equals("savings")){
+            userAnswer = " ";
+            System.out.println("Savings");
+        }
+        else if (userAnswer.equals("exit")) {
+            userAnswer = " ";
+            ask();
+        }
+        else {
+            System.out.print("[ERROR. PLEASE TRY AGAIN]");
+            userAnswer = " ";
+        }
+
+        }
+    public static void exit() {
+        if (userAnswer.equals("exit")) {
+            userAnswer = " ";
+            System.out.println("Thank you for banking with BaNK");
+            System.exit(0);
+        }
+    }
 
     }
 
